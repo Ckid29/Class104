d621450f5e3666a89d9597e9d54d856871430afb
Nathan Hundley <100338130+Ckid29@users.noreply.github.com>
2022-04-23 21:32:47 -0500

    added cards
    added cards and started on search, and delete

--- scripts/display.js
+++ scripts/display.js
@@ -1,19 +1,36 @@
 //display the pets in list items
 function displayPet(){
-    //creating the text
-    let petsDiv = document.getElementById("pets");
-    petsDiv.innerHTML="";
+//creating the text
+    let tmp="";//clear the field
     for(let i=0;i<petSalon.pets.length;i++){
         let pet = petSalon.pets[i];
-        tmp=`
-        <div class="pet">
-            <h4>${pet.name}</h4>
+        tmp+=`<li>${pet.name} --- ${pet.age}</li> `;
+        //concatenation
+        console.log(tmp);
+    }
+    document.getElementById("pets").innerHTML=tmp;
+}
+//display the pets in cards
+function displayCards(){
+    let tmp="";
+    for(let i=0;i<petsSalon.pets.length;i++){
+        let pet= petsSalon.pets[i];
+        tmp+=`
+        <div id="${pet.id}" class="pet">
+            <div class="pet-header">
+                <h4>${pet.name}</h4>
+                <button onclick="deletePet(${pet.id});">❤️‍🔥</button>
+            </div>
             <label>${pet.age}</label>
+            <label>${pet.gender}</label>
+            <label>${pet.breed}</label>
+            <label>${pet.service}</label>
+            <label>${pet.Owner}</label>
+            <label>${pet.Phone}</label>
         </div>
         `;
     }
     document.getElementById("pets").innerHTML=tmp;
 }
-//display the pets in cards
 
 //display the pets in table
--- scripts/register.js
+++ scripts/register.js
@@ -1,4 +1,4 @@
-let petSalon = {
+let petsSalon = {
     name:"The Fashion Pet",
     address:{
         street:"Ave University",
@@ -11,39 +11,114 @@ let petSalon = {
     },
     pets:[]
 }
+let c=0;//this is a counter var
 //name,age,gender,breed,service, owner name, contact phone
-function Pet(name,age, gender,breed,service,ownerName,contactPhone){
+function Pet(name,age, gender,breed,service,Owner,contactPhone){
     this.name=name;
     this.age=age;
     this.gender=gender;
     this.breed=breed;
     this.service=service;
-    this.owner=ownerName;
-    this.phone=contactPhone;
-    
+    this.owner=Owner;
+    this.phone=contactPhone;  
+    this.id=c++; //increase the var 1 everytime that is used
 }
+
 //get the info from the inputs and store the information
 let inputName=document.getElementById("txtName");
 let inputAge=document.getElementById("txtAge");
 let inputGender=document.getElementById("txtGender");
 let inputBreed=document.getElementById("txtBreed");
-let inputservice=document.getElementById("txtOwner");
+let inputService=document.getElementById("selService");
+let inputOwner=document.getElementById("txtOwner");
 let inputPhone=document.getElementById("txtTel"); 
 
+function isValid(aPet){
+    
+    let valid=true;
+    if(aPet.name.length==0){
+        //if we get here it means that the name is not valid
+       valid=false;
+       console.error("Invalid name");
+    }
+    if(aPet.service.length==0){
+        valid=false;
+        console.error("Invalid name");
+    }
+    if(aPet.phone.length==0){
+        valid=false;
+        console.error("Invalid name");
+    }
+    return valid;
+}
+
 function register(){
-    let thePet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputOwner.value,inputPhone.value);
-    console.log(thePet);
+    let thePet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputOwner.value,inputcontactPhone.value);
+    if(isValid(thePet)){
+        petsSalon.pets.push(thePet);
+        displayCards();
 
-    petSalon.pets.push(thePet);
-    clearInputs();
+        clearInputs();
+    }
+
 }
+
 function clearInputs(){
-    inputName.value="";
+    inputname.value="";
     inputAge.value="";
+    inputGender.value="";
+    inputBreed.value="";
+    inputService.value="";
+    inputOwner.value="";
+    inputPhone.value="";
 }
-let Janet= new Pet("Janet", "18wks", "male", "germansheppherd", "vet", "Johnathan", "2069452064");
-let Regina= new Pet("Regina", "9wks", "female", "Greyhound", "yes", "Shakira", "2533301698");
-let Gretchen= new Pet("Gretchen", "3wks", "male", "Bulldog", "yes", "Usher", "8566253039");
 
-console.log(Janet,Regina,Gretchen);
+function deletePet(petsId){
+    console.log("Deleting" + petsId);
+    let deleteIndex;
+    //previous actions----
+        //we need an id in the pet constructor
+        //we need a delete button in the html
+    for(let i=0;i<petsSalon.pets.length;i++){
+        let pet = petsSalon.pets[i];
+        if(petsId==pets.id){
+
+            deleteIndex=i;
+            console.log("I found it in position:" + i);
+        }
+    }
+    //in this function ------
+        //travel the array (for loop)
+        //find the id (if)
+        //get the position in the array (store in a variable)
+        //remove from the array(splice))
+        petsSalon.pets.splice(deleteIndex,1);
+        //remove from the html (remove))
+        document.getElementById(petsId).remove();
+        //display to the user a message
+    //    
+}
+
+function searchPet(){
+   
+    // in this function -------
+        //travel the array(for loop)
+        let searchString = document.getElementById("txtSearch").value;
+        console.log("Searching" + searchString);
+    for(let i=0;i<petsSalon.pets.length;i++){
+        let pets = petsSalon.pets[i];
+        // find the id (if)
+        if(searchString.toLowerCase() === pet.name.toLowerCase()){
+            //highlight the result
+            document.getElementById(pets.id).classList.add("highlight");
+        }else{
+            document.getElementById(pets.id).classList.remove("highlight");
+        }
+    }
+}
+let Janet= new Pet("Janet", "18", "male", "germansheppherd", "vet", "Johnathan", "2069452064");
+let Regina= new Pet("Regina", "9", "female", "Greyhound", "yes", "Shakira", "2533301698");
+let Gretchen= new Pet("Gretchen", "3", "male", "Bulldog", "yes", "Usher", "8566253039");
+petsSalon.pets.push(Janet,Regina,Gretchen);
+displayCards();
 //Create three pets
